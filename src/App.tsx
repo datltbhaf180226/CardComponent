0db73b80ac0de102f52ee1d0e7477bc0c9005b36
Dat Le <letrongdat487@gmail.com>
2021-04-06 19:08:17 +0700

    HW-B2

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Card from './card';
+import { FoodList } from './FoodList';
 
 function App() {
+   const BasicList : FoodList[] = [
+        {
+            content: '01 Seat',
+            isChecked: true
+        },
+        {
+            content: 'Tea & Coffee Breaks',
+            isChecked: false
+        },
+        {
+            content: 'Wifi Available',
+            isChecked: false
+        },
+        {
+            content: 'Exclusive Seatings',
+            isChecked: false
+        }
+   ];
+   const StandardList : FoodList[] = [
+        {
+            content: '01 Seat',
+            isChecked: true
+        },
+        {
+            content: 'Tea & Coffee Breaks',
+            isChecked: true
+        },
+        {
+            content: 'Wifi Available',
+            isChecked: false
+        },
+        {
+            content: 'Exclusive Seatings',
+            isChecked: false
+        }
+   ];
+  
+  const PremiumList : FoodList[] = [
+        {
+            content: '01 Seat',
+            isChecked: true
+        },
+        {
+            content: 'Tea & Coffee Breaks',
+            isChecked: true
+        },
+        {
+            content: 'Wifi Available',
+            isChecked: true
+        },
+        {
+            content: 'Exclusive Seatings',
+            isChecked: true
+        }
+    ];
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <Card title="Basic" icon="fas fa-bicycle" price="$100" taxes="Including of taxes" contentList={BasicList} primaryColor="#A31E7F" />
+        <Card title="Standard" icon="fas fa-car-alt" price="$200" taxes="Including of taxes" contentList={StandardList} primaryColor="#A31E7F" />
+        <Card title="Premium" icon="fas fa-rocket" price="$300" taxes="Including of taxes" contentList={PremiumList} primaryColor="#A31E7F" />   
+      </div>
   );
 }
 
